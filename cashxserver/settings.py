b668b6e14a3c3bb557028e8f0fa03f3e86c5e963
Emilio Israel Wong González <emilews.ew@gmail.com>
2019-12-25 21:55:49 -0700

    Correction: new endpoint in '/api/'

--- cashxserver/settings.py
+++ cashxserver/settings.py
@@ -123,5 +123,6 @@ STATIC_URL = '/static/'
 import django_heroku
 django_heroku.settings(locals())
 
+
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
